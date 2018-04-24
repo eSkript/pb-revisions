ddf361f98efd7e40af48e93427a80cd55ab56f95
Lukas <Kaiser>
2018-04-24 21:13:29 +0200

    Nicer comment type selection

--- admin/partials/pb-revisions-menu-page-basic-or-chapter-comments.php
+++ admin/partials/pb-revisions-menu-page-basic-or-chapter-comments.php
@@ -6,12 +6,12 @@ $summary_url = get_admin_url( get_current_blog_id(), "/admin.php?page=pb_revisio
 ?>
 <div class="wrap">
 	<h1><?php _e('What type of comments do you want to add?', 'pb-revisions');?></h1>
-	<div>
+	<div class="pb_revisions_button_list">
 		<?php if(!empty($data['first_chapter'])){
 			$next_chapter_url = get_admin_url( get_current_blog_id(), "/admin.php?page=pb_revisions&pb_revisions_view=chapter_diff&pb_revisions_version={$data['version']->ID}&pb_revisions_chapter={$data['first_chapter']->chapter}" );
 		?>
-			<a class="button button-hero button-primary generate right" href=<?php echo esc_url($next_chapter_url);?>><?php _e('Chapter Specific Comments', 'pb-revisions');?></a>
+			<a class="button button-hero button-primary generate pb_revisions_button_list__button" href=<?php echo esc_url($next_chapter_url);?>><?php _e('Chapter Specific Comments', 'pb-revisions');?></a>
 		<?php } ?>
-		<a class="button button-hero button-primary generate right" href=<?php echo esc_url($summary_url);?>><?php _e('Just One Comment For Everything', 'pb-revisions');?></a>	
+		<a class="button button-hero button-primary generate pb_revisions_button_list__button" href=<?php echo esc_url($summary_url);?>><?php _e('Just One Comment For Everything', 'pb-revisions');?></a>	
 	</div>
 </div>
