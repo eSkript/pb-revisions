42ca434609c1efef6cbc968bfb39c7f5061bef42
Lukas <Kaiser>
2018-05-28 16:22:40 +0200

    Remove deactivate from info header and change toggle button to red

--- public/js/button.js
+++ public/js/button.js
@@ -1,8 +1,8 @@
 (function ($) {
     $( document ).ready(function() {
         if(PBRevisionsButton.is_on){
-            $(".a11y-toolbar ul").append('<li><a href="'+PBRevisionsButton.hide_url+'" role="button" title="'+PBRevisionsButton._deactivatePreview+'"><span class="dashicons pbricons-revision"></span></a></li>');
-            $("body").prepend('<div class="notice">'+PBRevisionsButton._previewNotice+' <a href="'+PBRevisionsButton.hide_url+'" role="button" title="'+PBRevisionsButton._deactivatePreview+'">'+PBRevisionsButton._deactivate+'</a></div>');
+            $(".a11y-toolbar ul").append('<li><a href="'+PBRevisionsButton.hide_url+'" role="button" title="'+PBRevisionsButton._deactivatePreview+'" class="pb_revisions_toggle_button--on"><span class="dashicons pbricons-revision pbricons-revision--white"></span></a></li>');
+            $("body").prepend('<div class="notice">'+PBRevisionsButton._previewNotice+'</div>');
         }else {
             $(".a11y-toolbar ul").append('<li><a href="'+PBRevisionsButton.show_url+'" role="button" title="'+PBRevisionsButton._showPreview+'"><span class="dashicons dashicons-controls-play"></span></a></li>');
         }
